c73108a2adb228e97c3be2dc60643ed1767ef19a
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-12-01 11:48:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/yo1000/ddd/DddBootApplication.kt
@@ -0,0 +1,11 @@
+package com.yo1000.ddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddBootApplication>(*args)
+}
